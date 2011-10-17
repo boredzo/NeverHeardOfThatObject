95ac0ef038cc9c29c7c9c485b4644fd741b42fa8
Peter Hosey <prh@Featherweight.(none)>
2011-10-17 09:27:28 -0700

    Initial Commit

---
+++ NeverHeardOfThatObject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NeverHeardOfThatObject
+//
+//  Created by Peter Hosey on 2011-10-17.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
